a1fc8d9ab48abf043983fff508edc5de5afe9362
Raj Sekhar <kolakalurivijay91@gmail.com>
2020-06-24 21:52:03 +0530

    removed junk code

--- src/app/list.ts
+++ src/app/list.ts
@@ -5,18 +5,3 @@ export interface List {
   discount:number;
   img_url:string
 }
-
-
-
-    // <div *ngFor="let item of lists">
-	// 	<div class="col col-example">
-	// 		<div class="card" >
-	// 			<div class="card-body">
-    //       <img src="{{item.img_url}}">
-	// 				<h5 class="card-title">{{item.name}}</h5>
-	// 				<p class="card-text">{{item.price}}</p>
-	// 				<p class="card-text">{{item.discount}}</p>
-	// 				<a href="#" class="btn btn-primary">Go somewhere</a>
-	// 			</div>
-	// 		</div>
-	// 	</div>
